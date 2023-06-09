5218291b77f8fe80b9218e0d1d1b72cca7671077
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-06-09 19:25:30 +0900

    ジャッカル猫のオプション適用はジャッカルのものをそのまま使うよう変更

--- Roles/Neutral/Jackal.cs
+++ Roles/Neutral/Jackal.cs
@@ -63,9 +63,6 @@ namespace TownOfHostY.Roles.Neutral
         }
         public override bool OnInvokeSabotage(SystemTypes systemType) => CanUseSabotage;
         public override bool CanSabotage(SystemTypes systemType) => CanUseSabotage;
-        public void ApplySchrodingerCatOptions(IGameOptions option)
-        {
-            option.SetVision(HasImpostorVision);
-        }
+        public void ApplySchrodingerCatOptions(IGameOptions option) => ApplyGameOptions(option);
     }
 }
