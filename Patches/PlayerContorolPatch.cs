ed2bcd6c944bcbefdc84a598d50a250cc2610834
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-06-22 16:18:02 +0900

    CA1829(CountまたはLengthプロパティを使える型でCount()メソッドを使用している)を解消

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -190,7 +190,7 @@ namespace TownOfHostY
                         mpdistance.Add(p, dis);
                     }
                 }
-                if (mpdistance.Count() != 0)
+                if (mpdistance.Count != 0)
                 {
                     var min = mpdistance.OrderBy(c => c.Value).FirstOrDefault();//一番値が小さい
                     PlayerControl targetm = min.Key;
