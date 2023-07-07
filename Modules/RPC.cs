69863e1546d1e62151cc7a6e37769401bd438255
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-07-07 17:38:02 +0900

    相方のキル時に場所の通知を受け取る能力を作成

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -35,6 +35,7 @@ namespace TownOfHostY
         SyncPuppet,
         MareSync,
         SetSchrodingerCatTeam,
+        EvilHackerCreateMurderNotify,
 
         SetOppoKillerShotLimit,
         SetCursedWolfSpellCount,
