3079d4595598f0adb14b76a93b128c59fbd48852
Masami <103577031+Masami4711@users.noreply.github.com>
2023-08-04 19:18:16 +0900

    GetLowerTextのisForMeetingを正常に反映

--- Patches/HudPatch.cs
+++ Patches/HudPatch.cs
@@ -88,7 +88,7 @@ namespace TownOfHostY
                         LowerInfoText.fontSizeMax = 2.0f;
                     }
 
-                    LowerInfoText.text = roleClass?.GetLowerText(player, isForHud: true) ?? "";
+                    LowerInfoText.text = roleClass?.GetLowerText(player, isForMeeting: GameStates.IsMeeting, isForHud: true) ?? "";
                     LowerInfoText.enabled = LowerInfoText.text != "";
 
                     if (!AmongUsClient.Instance.IsGameStarted && AmongUsClient.Instance.NetworkMode != NetworkModes.FreePlay)
