967621e434b44c6891bf2e504274489305be4506
schwKatz <katz.schw@gmail.com>
2023-10-30 17:12:49 +0900

    bugfix)ホストがベント中に会議になった場合、湧き位置がベント位置になってしまう不具合修正

--- Patches/VentilationSystemPatch.cs
+++ Patches/VentilationSystemPatch.cs
@@ -15,8 +15,12 @@ class VentilationSystemPatch
     public static void ClearVent()
     {
         if (Instance == null) return;
+        if (Main.NormalOptions.MapId != (byte)MapNames.Skeld &&
+            Main.NormalOptions.MapId != (byte)MapNames.Mira &&
+            Main.NormalOptions.MapId != (byte)MapNames.Dleks &&
+            Main.NormalOptions.MapId != (byte)MapNames.Airship) return;
 
-        List<(byte playerId, byte ventId)> list = new();
+        List <(byte playerId, byte ventId)> list = new();
         foreach (var vent in Instance.PlayersInsideVents)
         {
             list.Add((vent.Key, vent.Value));
@@ -26,7 +30,16 @@ class VentilationSystemPatch
         {
             var player = Utils.GetPlayerById(vent.playerId);
             Logger.Info($"ClearVent player: {player?.name}, vent: {vent.ventId}", "VentilationSystem");
-            player?.MyPhysics.RpcExitVent(vent.ventId);
+            if (vent.playerId == PlayerControl.LocalPlayer.PlayerId)
+            {
+                MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)RpcCalls.ExitVent, SendOption.Reliable);
+                messageWriter.WritePacked(vent.ventId);
+                messageWriter.EndMessage();
+            }
+            else
+            {
+                player?.MyPhysics.RpcExitVent(vent.ventId);
+            }
         }
     }
 }
