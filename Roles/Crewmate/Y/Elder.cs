5db734b14ca68230376cf5e53eced5bd271f455d
Kihi1120 <hope.light1120@gmail.com>
2024-05-13 16:43:31 +0900

    killを防ぐ処理を修正 役職を変更する処理の追加。(仮です)

--- Roles/Crewmate/Y/Elder.cs
+++ Roles/Crewmate/Y/Elder.cs
@@ -36,6 +36,11 @@ public sealed class Elder : RoleBase
     }
     private static bool DiaInLife;
     int GuardCount = 0;
+    public static CustomRoles ChangeRoleElderDead;
+    public static readonly CustomRoles[] ChangeRoles =
+    {
+            CustomRoles.Crewmate,
+    };
     private static void SetupOptionItem()
     {
         OptionDiaInLife = BooleanOptionItem.Create(RoleInfo, 10, OptionName.ElderDiaInLife, false, false);
@@ -43,12 +48,30 @@ public sealed class Elder : RoleBase
     public override bool OnCheckMurderAsTarget(MurderInfo info)
     {
         (var killer, var target) = info.AttemptTuple;
-        if (GuardCount == 0) return true;//普通にキル
-        info.CanKill = false;
 
-        killer.RpcProtectedMurderPlayer(target);
-        GuardCount++;
-
+        if (GuardCount > 0)
+        {// GuardCountが1以上の場合はキルを通す
+            info.CanKill = true;
+        }
+        else
+        {// GuardCountが0の場合はキルを通さない。
+            info.CanKill = false;
+            killer.RpcProtectedMurderPlayer(target);
+            killer.SetKillCooldown();
+        }
+        GuardCount++;// GuardCountを増やす
         return true;
+    }
+    public void ChangeRole()
+    {
+        Player.RpcSetCustomRole(ChangeRoleElderDead);
+        Utils.NotifyRoles();
+    }
+    public override void OnFixedUpdate(PlayerControl player)
+    {
+        if (!Player.IsAlive())
+        {
+            ChangeRole();
+        }
     }
 }
