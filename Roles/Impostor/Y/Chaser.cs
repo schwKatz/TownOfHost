ce44314d831c39c6e36482bb0553783bb371656c
Kihi1120 <hope.light1120@gmail.com>
2024-06-14 11:54:05 +0900

    Chaserのオプション項目の追加。

--- Roles/Impostor/Y/Chaser.cs
+++ Roles/Impostor/Y/Chaser.cs
@@ -24,7 +24,24 @@ public sealed class Chaser : RoleBase, IImpostor, ISidekickable
     )
     {
     }
+    private static OptionItem OptionKillCooldown;
+    private static OptionItem OptionChaseCooldown;
+    private static OptionItem OptionReturnPosition;
+    private static OptionItem OptionPositionTime;
+    enum OptionName
+    {
+        ChaserChaseCooldown,
+        ChaserReturnPosition,
+        ChaserPositionTime,
+    }
     private static void SetUpOptionItem()
     {
+        OptionKillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(2.5f, 180f, 2.5f), 30f, false)
+                .SetValueFormat(OptionFormat.Seconds);
+        OptionChaseCooldown = FloatOptionItem.Create(RoleInfo, 11, OptionName.ChaserChaseCooldown, new(1f, 180f, 1f), 30f, false)
+                .SetValueFormat(OptionFormat.Seconds);
+        OptionReturnPosition = BooleanOptionItem.Create(RoleInfo, 12, OptionName.ChaserReturnPosition, false, false);
+        OptionPositionTime = FloatOptionItem.Create(RoleInfo, 13, OptionName.ChaserPositionTime, new(1f, 99f, 1f), 10f, false, OptionReturnPosition)
+            .SetValueFormat(OptionFormat.Seconds);
     }
 }
