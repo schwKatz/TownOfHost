c31ea4c3f01e283686a56a4e625afc1ce3ceec5b
Kihi1120 <hope.light1120@gmail.com>
2024-07-06 21:04:09 +0900

    SetUpOptionItemを追加。 OptionNameを追加。

--- Roles/Impostor/Y/CharisMastar.cs
+++ Roles/Impostor/Y/CharisMastar.cs
@@ -27,8 +27,43 @@ public sealed class CharisMastar : RoleBase, IImpostor, ISidekickable
         player
     )
     {
+        KillCooldown = OptionKillCooldown.GetFloat();
+        GatherCount = OptionGatherCount.GetInt();
+        GatherCooldown = OptionGatherCooldown.GetFloat();
+        NotGatherPlayerKill = OptionNotGatherPlayerKill.GetBool();
+        GathersMode = (GatherMode)OptionGatherMode.GetValue();
     }
+    public enum GatherMode
+    {
+        CanChoose,
+        EveryoneGather,
+    };
+    enum OptionName
+    {
+        CharisMastarGatherCount,
+        CharisMastarGatherCooldown,
+        CharisMastarNotGatherPlayerKill,
+        CharisMastarGatherMode,
+    }
+    private static float KillCooldown;
+    private static float GatherCooldown;
+    public static bool NotGatherPlayerKill;
+    private static OptionItem OptionKillCooldown;
+    private static OptionItem OptionGatherCount;
+    private static OptionItem OptionGatherCooldown;
+    private static OptionItem OptionNotGatherPlayerKill;
+    public static StringOptionItem OptionGatherMode;
+    public static GatherMode GathersMode;
+    int GatherCount;
     private static void SetUpOptionItem()
     {
+        OptionKillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(2.5f, 180f, 2.5f), 30f, false)
+                .SetValueFormat(OptionFormat.Seconds);
+        OptionGatherCount = IntegerOptionItem.Create(RoleInfo, 11, OptionName.CharisMastarGatherCount, new(1, 10, 1), 3, false)
+            .SetValueFormat(OptionFormat.Pieces);
+        OptionGatherCooldown = FloatOptionItem.Create(RoleInfo, 12, OptionName.CharisMastarGatherCooldown, new(5f, 900f, 5f), 30f, false)
+            .SetValueFormat(OptionFormat.Seconds);
+        OptionNotGatherPlayerKill = BooleanOptionItem.Create(RoleInfo, 13, OptionName.CharisMastarNotGatherPlayerKill, true, false);
+        OptionGatherMode = StringOptionItem.Create(RoleInfo, 14, OptionName.CharisMastarGatherMode, GatherModeText, 2, false);
     }
 }
