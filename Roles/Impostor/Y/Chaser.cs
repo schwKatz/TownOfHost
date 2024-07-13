9f71ff4531af19e3b641f7ec78962eb6056c452c
Yumenopai <sei0415tp@yahoo.co.jp>
2024-07-13 18:45:07 +0900

    チェイス時の変身クールリセットの追加 コードの若干修正

--- Roles/Impostor/Y/Chaser.cs
+++ Roles/Impostor/Y/Chaser.cs
@@ -15,7 +15,8 @@ public sealed class Chaser : RoleBase, IImpostor, ISidekickable
             CustomRoles.Chaser,
             () => RoleTypes.Shapeshifter,
             CustomRoleTypes.Impostor,
-            (int)Options.offsetId.ImpY + 1700,//仮
+            (int)Options.offsetId.ImpSpecial + 0,
+            //(int)Options.offsetId.ImpY + 1700,
             SetUpOptionItem,
             "チェイサー"
         );
@@ -38,7 +39,6 @@ public sealed class Chaser : RoleBase, IImpostor, ISidekickable
     private static float ChaseCooldown;
     public static bool ReturnPosition;
     private static float PositionTime;
-    private static bool IsVentWarp;//飛ばすフラグ
     private Vector2 lastTransformPosition;
     enum OptionName
     {
@@ -56,53 +56,65 @@ public sealed class Chaser : RoleBase, IImpostor, ISidekickable
         OptionPositionTime = FloatOptionItem.Create(RoleInfo, 13, OptionName.ChaserPositionTime, new(1f, 99f, 1f), 10f, false, OptionReturnPosition)
             .SetValueFormat(OptionFormat.Seconds);
     }
-    public override bool OnCheckShapeshift(PlayerControl target, ref bool animate)
-    {
-        IsVentWarp = true;
-        lastTransformPosition = Player.transform.position;          // 変身前の位置を記録する
 
-        Player.MyPhysics.RpcExitVent(GetNearestVent(target).Id);//ベントの位置へ飛ばす。
-        animate = false;
-        if (ReturnPosition)
-        {
-            MyState.CanUseMovingPlatform = false;                   //元の位置に戻る時はぬーんを使わせない。
-
-            _ = new LateTask(() =>
-            {
-                if (Player.MyPhysics.Animations.IsPlayingAnyLadderAnimation())
-                {//梯子をもし使っている場合
-                    Logger.Info($"梯子を使っていたため元の位置に戻れませんでした。", "Chaser");
-                    return;
-                }
-                else
-                {
-                    var returnVent = GetReturnNearestVent();
-                    if (IsVentWarp)
-                    {
-                        Player.MyPhysics.RpcExitVent(returnVent.Id);        // 変身前の位置にプレイヤーを戻す
-                        IsVentWarp = false;
-                    }
-                    lastTransformPosition = default;                            //位置をリセットする。
-                    MyState.CanUseMovingPlatform = true;                        //元通りぬーんを使えるようにする。
-
-                }
-            }, PositionTime, "ReturnPosition");
-        }
-        return false;
-    }
-    Vent GetNearestVent(PlayerControl target)
-    {
-        var vents = ShipStatus.Instance.AllVents.OrderBy(v => (target.transform.position - v.transform.position).magnitude);
-        return vents.First();
-    }
-    Vent GetReturnNearestVent()
-    {
-        var vents = ShipStatus.Instance.AllVents.OrderBy(v => (lastTransformPosition - (Vector2)v.transform.position).magnitude);
-        return vents.First();
-    }
+    public float CalculateKillCooldown() => KillCooldown;
     public override void ApplyGameOptions(IGameOptions opt)
     {
         AURoleOptions.ShapeshifterCooldown = ChaseCooldown;
     }
-    public float CalculateKillCooldown() => KillCooldown;
+    public override void AfterMeetingTasks()
+    {
+        if (Player.IsAlive()) Player.RpcResetAbilityCooldown();
+    }
+
+    public override bool OnCheckShapeshift(PlayerControl target, ref bool animate)
+    {
+        // 変身アニメーションを起こさない
+        animate = false;
+        // 変身前の位置を記録する
+        lastTransformPosition = Player.transform.position;
+        // ベントの位置へ飛ばす。
+        Player.MyPhysics.RpcExitVent(GetNearestVentId(target));
+        // 変身クールダウンのリセット
+        Player.RpcResetAbilityCooldown();
+
+        // 以下、元の場所に戻れる時の処理
+        if (!ReturnPosition) return false;
+
+        // 元の位置に戻る時はぬーんを使わせない。
+        MyState.CanUseMovingPlatform = false;                   
+
+        // 戻る時間後の処理
+        _ = new LateTask(() =>
+        {
+            // もし梯子を使っている場合
+            if (Player.MyPhysics.Animations.IsPlayingAnyLadderAnimation())
+            {
+                Logger.Info($"梯子を使っていたため元の位置に戻れませんでした。", "Chaser");
+                return;
+            }
+
+            // 元居た場所に近いベントの位置にプレイヤーを戻す
+            Player.MyPhysics.RpcExitVent(GetReturnNearestVentId());
+            // 位置をリセットする
+            lastTransformPosition = default;
+            // 元通りぬーんを使えるようにする
+            MyState.CanUseMovingPlatform = true;
+
+        }, PositionTime, "ReturnPosition");
+        
+        return false;
+    }
+    // ターゲットから一番近いベントを探す
+    int GetNearestVentId(PlayerControl target)
+    {
+        var vents = ShipStatus.Instance.AllVents.OrderBy(v => (target.transform.position - v.transform.position).magnitude);
+        return vents.First().Id;
+    }
+    // 戻る時の一番近いベントを探す
+    int GetReturnNearestVentId()
+    {
+        var vents = ShipStatus.Instance.AllVents.OrderBy(v => (lastTransformPosition - (Vector2)v.transform.position).magnitude);
+        return vents.First().Id;
+    }
 }
